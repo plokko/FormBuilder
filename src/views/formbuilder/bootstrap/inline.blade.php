8522689088fe493285c7b58c4be5bd8ea4cc6494
Plokko <plo_koon@email.it>
2016-01-21 11:54:32 +0100

    minor bootstrap view fixes

--- src/views/formbuilder/bootstrap/inline.blade.php
+++ src/views/formbuilder/bootstrap/inline.blade.php
@@ -2,19 +2,21 @@
 
     <div class="form-group">
         {!! $field->renderLabel(['class'=>'col-sm-2 control-label']) !!}
-        <div class="col-sm-8">
-        @if(!$field->isGroup())
-                {!!
-                    ($field->type=='checkbox'||$field->type=='radio')?
-                        $field://No form-control for input or radios
-                        $field->addClass('form-control')
-                !!}
-        @else
-            @foreach($field AS $f)
-                {!! $f->renderLabel() !!}
-                {!! $f->addClass('form-control') !!}
-            @endforeach
-        @endif
+        <div class="row">
+            <div class="col-sm-8">
+            @if(!$field->isGroup())
+                    {!!
+                        ($field->type=='checkbox'||$field->type=='radio')?
+                            $field://No form-control for input or radios
+                            $field->addClass('form-control')
+                    !!}
+            @else
+                @foreach($field AS $f)
+                    {!! $f->renderLabel() !!}
+                    {!! $f->addClass('form-control') !!}
+                @endforeach
+            @endif
+            </div>
         </div>
     </div>
 @endforeach
